e6db7274db8a21c6025fee0e85799f934a5f195f
20172104254 <15750545478@163.com>
2018-06-25 10:07:43 +0800

    纯虚函数
    添加说明以便理解

--- duotai2/duotai2.cpp
+++ duotai2/duotai2.cpp
@@ -22,6 +22,7 @@ public:
 	}//如不把纯虚函数写出来,派生类是不能创建对象的
 	void display()
 	{
+		//CBace::display();
 		cout << "paishenglei" << endl;
 	}//函数覆盖
 };
@@ -32,7 +33,7 @@ int main()
 	CBace *p;//抽象类不能实例化,就是不能创建对象,是用来继承的
 	CDervite obj1;
 	p = &obj1;//基类指针能指向派生类对象
-	p->display();
+	p->display();//动态绑定
 	p->CBace::display();//预作用预符
     return 0;
 }
